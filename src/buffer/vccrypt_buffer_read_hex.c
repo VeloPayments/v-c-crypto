28e3638971ff4eb8cc1ec2f980b367d98f20fb27
Justin Handville <nanolith@gmail.com>
2017-03-11 17:26:09 -0500

    Added hex read capability.

---
+++ src/buffer/vccrypt_buffer_read_hex.c
@@ -0,0 +1,111 @@
+/**
+ * \file buffer/vccrypt_buffer_read_hex.c
+ *
+ * Read the hexadecimal representation of a buffer into a byte buffer.
+ *
+ * \copyright 2017 Velo Payments, Inc.  All rights reserved.
+ */
+
+#include <cbmc/model_assert.h>
+#include <string.h>
+#include <vccrypt/buffer.h>
+#include <vpr/parameters.h>
+
+/* forward decls */
+static uint8_t from_hex(uint8_t hex);
+
+/**
+ * Read buffer data from hex.
+ *
+ * Note: buffers must be sized appropriately.
+ *
+ * \param dest      the destination byte buffer.
+ * \param source    the source hex buffer.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int vccrypt_buffer_read_hex(
+    vccrypt_buffer_t* dest, const vccrypt_buffer_t* source)
+{
+    MODEL_ASSERT(dest != NULL);
+    MODEL_ASSERT(dest->data != NULL);
+    MODEL_ASSERT(source != NULL);
+    MODEL_ASSERT(source->data != NULL);
+    MODEL_ASSERT(dest->size > 0);
+    MODEL_ASSERT(source->size > 0);
+    MODEL_ASSERT(source->size % 2 == 0);
+    MODEL_ASSERT(dest->size >= source->size / 2);
+
+    /* we can't exceed the destination buffer size */
+    if (dest->size < source->size / 2)
+        return 1;
+
+    /* convert data pointers to byte buffers. */
+    uint8_t* out = (uint8_t*)dest->data;
+    const uint8_t* in = (uint8_t*)source->data;
+
+    /* iterate through each byte of the input buffer. */
+    for (size_t i = 0; i < source->size; i += 2)
+    {
+        *out++ = (from_hex(in[i]) << 4) | from_hex(in[i + 1]);
+    }
+
+    /* success */
+    return 0;
+}
+
+/**
+ * Given a hex digit, return the nibble value associated with this value.
+ *
+ * \param hex        the hex digit value.
+ *
+ * \returns the saturated nibble representation of this digit.
+ */
+static uint8_t from_hex(uint8_t hex)
+{
+    MODEL_ASSERT(
+        (hex >= '0' && hex <= '9') ||
+        (hex >= 'A' && hex <= 'F') ||
+        (hex >= 'a' && hex <= 'f'));
+
+    switch (hex)
+    {
+        case '0':
+            return 0x00;
+        case '1':
+            return 0x01;
+        case '2':
+            return 0x02;
+        case '3':
+            return 0x03;
+        case '4':
+            return 0x04;
+        case '5':
+            return 0x05;
+        case '6':
+            return 0x06;
+        case '7':
+            return 0x07;
+        case '8':
+            return 0x08;
+        case '9':
+            return 0x09;
+        case 'A':
+        case 'a':
+            return 0x0A;
+        case 'B':
+        case 'b':
+            return 0x0B;
+        case 'C':
+        case 'c':
+            return 0x0C;
+        case 'D':
+        case 'd':
+            return 0x0D;
+        case 'E':
+        case 'e':
+            return 0x0E;
+        default:
+            return 0x0F;
+    }
+}
---
+++ test/buffer/test_vccrypt_buffer_read_hex.cpp
@@ -0,0 +1,92 @@
+/**
+ * \file test_vccrypt_buffer_read_hex.cpp
+ *
+ * Unit tests for vccrypt_buffer_read_hex.
+ *
+ * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ */
+
+#include <gtest/gtest.h>
+#include <vpr/allocator/malloc_allocator.h>
+#include <vccrypt/buffer.h>
+
+class vccrypt_buffer_read_hex_test : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        malloc_allocator_options_init(&alloc_opts);
+    }
+
+    void TearDown() override
+    {
+        dispose((disposable_t*)&alloc_opts);
+    }
+
+    allocator_options_t alloc_opts;
+};
+
+/**
+ * Test that we can read hex values from an input buffer.
+ */
+TEST_F(vccrypt_buffer_read_hex_test, simple_test)
+{
+    size_t BUFFER_SIZE = 32;
+    vccrypt_buffer_t source, dest;
+
+    //create source buffer
+    ASSERT_EQ(0,
+        vccrypt_buffer_init_for_hex_serialization(
+            &source, &alloc_opts, BUFFER_SIZE));
+
+    //set data in source buffer
+    memcpy(source.data, "000102030405060708090A0B0C0D0E0F"
+                        "101112131415161718191A1B1C1D1E1F",
+        64);
+
+    //create destination buffer
+    ASSERT_EQ(0, vccrypt_buffer_init(&dest, &alloc_opts, BUFFER_SIZE));
+
+    //read hex digits from the input buffer, and write bytes to the output
+    //buffer.
+    ASSERT_EQ(0, vccrypt_buffer_read_hex(&dest, &source));
+
+    //verify the byte values
+    uint8_t* dest_bytes = (uint8_t*)dest.data;
+    for (size_t i = 0; i < 16; ++i)
+    {
+        EXPECT_EQ((uint8_t)i, dest_bytes[i]);
+        EXPECT_EQ((uint8_t)i + 16, dest_bytes[i + 16]);
+    }
+
+    //dispose buffers
+    dispose((disposable_t*)&source);
+    dispose((disposable_t*)&dest);
+}
+
+/**
+ * Test that an error is returned when hex data is read into a destination
+ * buffer too small to hold the data.
+ */
+TEST_F(vccrypt_buffer_read_hex_test, size_mismtach)
+{
+    size_t BUFFER_SIZE = 32;
+    vccrypt_buffer_t source, dest;
+
+    //create source buffer
+    ASSERT_EQ(0,
+        vccrypt_buffer_init_for_hex_serialization(
+            &source, &alloc_opts, BUFFER_SIZE));
+
+    //set the buffer to valid hex data
+    memset(source.data, '0', BUFFER_SIZE * 2);
+
+    //create destination buffer that is too small
+    ASSERT_EQ(0, vccrypt_buffer_init(&dest, &alloc_opts, BUFFER_SIZE - 1));
+
+    //an attempt to read hex digits from the source buffer will fail.
+    EXPECT_NE(0, vccrypt_buffer_read_hex(&dest, &source));
+
+    //dispose buffers
+    dispose((disposable_t*)&source);
+    dispose((disposable_t*)&dest);
+}
