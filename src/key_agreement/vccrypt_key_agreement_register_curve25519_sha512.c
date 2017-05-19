3380eca5ac193d08f841f9b5d2ff8539db081919
Justin Handville <nanolith@gmail.com>
2017-05-18 23:09:19 -0400

    Added Curve25519 / SHA-512.

---
+++ src/key_agreement/vccrypt_key_agreement_register_curve25519_sha512.c
@@ -0,0 +1,248 @@
+/**
+ * \file vccrypt_key_agreement_register_curve25519_sha512.c
+ *
+ * Register sha512 curve25519 and force a link dependency so that this algorithm
+ * can be used at runtime.
+ *
+ * \copyright 2017 Velo Payments, Inc.  All rights reserved.
+ */
+
+#include <cbmc/model_assert.h>
+#include <stdbool.h>
+#include <string.h>
+#include <vccrypt/key_agreement.h>
+#include <vccrypt/hash.h>
+#include <vccrypt/mac.h>
+#include <vpr/abstract_factory.h>
+#include <vpr/allocator.h>
+#include <vpr/parameters.h>
+
+#include "../digital_signature/ref/curve25519.h"
+
+/* forward decls */
+static int vccrypt_curve25519_sha512_init(void* options, void* context);
+static void vccrypt_curve25519_sha512_dispose(void* options, void* context);
+static int vccrypt_curve25519_sha512_long_term_secret_create(
+    void* context, const vccrypt_buffer_t* priv,
+    const vccrypt_buffer_t* pub, vccrypt_buffer_t* shared);
+static int vccrypt_curve25519_sha512_keypair_create(
+    void* context, vccrypt_buffer_t* priv, vccrypt_buffer_t* pub);
+
+/* static data for this instance */
+static abstract_factory_registration_t curve25519_sha512_impl;
+static vccrypt_key_agreement_options_t curve25519_sha512_options;
+static bool curve25519_sha512_impl_registered = false;
+
+/**
+ * Register curve25519_sha512 for use by the crypto library.
+ */
+void vccrypt_key_agreement_register_curve25519_sha512()
+{
+    MODEL_ASSERT(!curve25519_sha512_impl_registered);
+
+    /* only register once */
+    if (curve25519_sha512_impl_registered)
+    {
+        return;
+    }
+
+    /* we need HMAC-SHA-512 for curve25519_sha512 */
+    vccrypt_mac_register_SHA_2_512_HMAC();
+
+    /* set up the options for curve25519_sha512 */
+    curve25519_sha512_options.hdr.dispose = 0; /* disposal handled by init */
+    curve25519_sha512_options.alloc_opts = 0; /* allocator handled by init */
+    curve25519_sha512_options.prng_opts = 0; /* prng options handled by init */
+    curve25519_sha512_options.hash_algorithm =
+        VCCRYPT_HASH_ALGORITHM_SHA_2_512;
+    curve25519_sha512_options.hmac_algorithm =
+        VCCRYPT_MAC_ALGORITHM_SHA_2_512_HMAC;
+    curve25519_sha512_options.shared_secret_size = 64;
+    curve25519_sha512_options.private_key_size = 32;
+    curve25519_sha512_options.public_key_size = 32;
+    curve25519_sha512_options.minimum_nonce_size = 64;
+    curve25519_sha512_options.vccrypt_key_agreement_alg_init =
+        &vccrypt_curve25519_sha512_init;
+    curve25519_sha512_options.vccrypt_key_agreement_alg_dispose =
+        &vccrypt_curve25519_sha512_dispose;
+    curve25519_sha512_options.vccrypt_key_agreement_alg_long_term_secret_create =
+        &vccrypt_curve25519_sha512_long_term_secret_create;
+    curve25519_sha512_options.vccrypt_key_agreement_alg_keypair_create =
+        &vccrypt_curve25519_sha512_keypair_create;
+
+    /* set up this registration for the abstract factory */
+    curve25519_sha512_impl.interface = VCCRYPT_INTERFACE_KEY;
+    curve25519_sha512_impl.implementation =
+        VCCRYPT_KEY_AGREEMENT_ALGORITHM_CURVE25519_SHA512;
+    curve25519_sha512_impl.implementation_features =
+        VCCRYPT_KEY_AGREEMENT_ALGORITHM_CURVE25519_SHA512;
+    curve25519_sha512_impl.factory = 0;
+    curve25519_sha512_impl.context = &curve25519_sha512_options;
+
+    /* register this instance */
+    abstract_factory_register(&curve25519_sha512_impl);
+
+    /* only register once */
+    curve25519_sha512_impl_registered = true;
+}
+
+/**
+ * Algorithm-specific initialization for key agreement.
+ *
+ * \param options   Opaque pointer to this options structure.
+ * \param context   Opaque pointer to the vccrypt_key_agreement_context_t
+ *                  structure.
+ *
+ * \returns 0 on success and non-zero on error.
+ */
+static int vccrypt_curve25519_sha512_init(void* UNUSED(options), void* context)
+{
+    vccrypt_key_agreement_context_t* ctx =
+        (vccrypt_key_agreement_context_t*)context;
+    MODEL_ASSERT(ctx != NULL);
+
+    /* we don't need separate state for sha512 mode */
+    ctx->key_agreement_state = NULL;
+
+    /* success */
+    return 0;
+}
+
+/**
+ * Algorithm-specific disposal for key agreement.
+ *
+ * \param options   Opaque pointer to this options structure.
+ * \param context   Opaque pointer to the vccrypt_key_agreement_context_t
+ *                  structure.
+ */
+static void vccrypt_curve25519_sha512_dispose(
+    void* UNUSED(options), void* UNUSED(context))
+{
+    /* no special cleanup needed */
+}
+
+/**
+ * Generate the long-term secret, given a private key and a public key.
+ *
+ * \param context   Opaque pointer to the vccrypt_key_agreement_context_t
+ *                  structure.
+ * \param priv      The private key to use for this operation.
+ * \param pub       The public key to use for this operation.
+ * \param shared    The buffer to receive the long-term secret.
+ *
+ * \returns 0 on success and non-zero on error.
+ */
+static int vccrypt_curve25519_sha512_long_term_secret_create(
+    void* context, const vccrypt_buffer_t* priv,
+    const vccrypt_buffer_t* pub, vccrypt_buffer_t* shared)
+{
+    int retval = 1;
+    vccrypt_key_agreement_context_t* ctx =
+        (vccrypt_key_agreement_context_t*)context;
+    MODEL_ASSERT(ctx != NULL);
+    MODEL_ASSERT(ctx->options != NULL);
+    MODEL_ASSERT(priv != NULL);
+    MODEL_ASSERT(priv->data != NULL);
+    MODEL_ASSERT(priv->size == X25519_KEY_LENGTH);
+    MODEL_ASSERT(pub != NULL);
+    MODEL_ASSERT(pub->data != NULL);
+    MODEL_ASSERT(pub->size == X25519_KEY_LENGTH);
+    MODEL_ASSERT(shared != NULL);
+    MODEL_ASSERT(shared->data != NULL);
+    MODEL_ASSERT(shared->size == ctx->options->shared_secret_size);
+
+    /* create the buffer for holding the long term secret from curve */
+    vccrypt_buffer_t ltprime;
+    retval = vccrypt_buffer_init(
+        &ltprime, ctx->options->alloc_opts, X25519_KEY_LENGTH);
+    if (0 != retval)
+    {
+        return retval;
+    }
+
+    /* generate the curve25519 long term secret */
+    retval = X25519(ltprime.data, priv->data, pub->data);
+    if (0 != retval)
+    {
+        goto dispose_ltprime;
+    }
+
+    /* create a hash options instance */
+    vccrypt_hash_options_t hash_opts;
+    retval = vccrypt_hash_options_init(
+        &hash_opts, ctx->options->alloc_opts,
+        ctx->options->hash_algorithm);
+    if (0 != retval)
+    {
+        goto dispose_ltprime;
+    }
+
+    /* create hash instance */
+    vccrypt_hash_context_t hash;
+    retval = vccrypt_hash_init(&hash_opts, &hash);
+    if (0 != retval)
+    {
+        goto dispose_hash_opts;
+    }
+
+    /* digest the curve25519 long term secret */
+    retval = vccrypt_hash_digest(&hash, ltprime.data, ltprime.size);
+    if (0 != retval)
+    {
+        goto dispose_hash;
+    }
+
+    /* finalize the hash */
+    retval = vccrypt_hash_finalize(&hash, shared);
+
+    /* fall-through */
+
+dispose_hash:
+    dispose((disposable_t*)&hash);
+
+dispose_hash_opts:
+    dispose((disposable_t*)&hash_opts);
+
+dispose_ltprime:
+    dispose((disposable_t*)&ltprime);
+
+    return retval;
+}
+
+/**
+ * Generate a keypair.
+ *
+ * \param context   Opaque pointer to the vccrypt_key_agreement_context_t
+ *                  structure.
+ * \param priv      The buffer to receive the private key.
+ * \param pub       The buffer to receive the public key.
+ *
+ * \returns 0 on success and non-zero on error.
+ */
+static int vccrypt_curve25519_sha512_keypair_create(
+    void* context, vccrypt_buffer_t* priv, vccrypt_buffer_t* pub)
+{
+    vccrypt_key_agreement_context_t* ctx =
+        (vccrypt_key_agreement_context_t*)context;
+    MODEL_ASSERT(ctx != NULL);
+    MODEL_ASSERT(ctx->options != NULL);
+    MODEL_ASSERT(ctx->options->prng_opts != NULL);
+    int retval = 0;
+
+    /* create a PRNG context for use by the keypair algorithm */
+    vccrypt_prng_context_t prng_ctx;
+    retval = vccrypt_prng_init(ctx->options->prng_opts, &prng_ctx);
+    if (0 != retval)
+    {
+        return retval;
+    }
+
+    /* generate the keypair */
+    retval =
+        X25519_keypair(pub->data, priv->data, &prng_ctx);
+
+    /* dispose of the prng */
+    dispose((disposable_t*)&prng_ctx);
+
+    return retval;
+}
---
+++ test/key_agreement/test_vccrypt_curve25519_sha512_ref.cpp
@@ -0,0 +1,347 @@
+/**
+ * \file test_vccrypt_curve25519_sha512_ref.cpp
+ *
+ * Unit tests for the reference curve25519 "sha512" implementation.
+ *
+ * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ */
+
+#include <fstream>
+#include <sstream>
+
+#include <gtest/gtest.h>
+#include <vccrypt/key_agreement.h>
+#include <vpr/allocator/malloc_allocator.h>
+
+using namespace std;
+
+class vccrypt_curve25519_sha512_ref_test : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        //make sure our key agreement algorithm has been registered
+        vccrypt_key_agreement_register_curve25519_sha512();
+        //make sure the prng has been registered
+        vccrypt_prng_register_source_operating_system();
+
+        malloc_allocator_options_init(&alloc_opts);
+
+        vccrypt_prng_options_init(
+            &prng_opts, &alloc_opts, VCCRYPT_PRNG_SOURCE_OPERATING_SYSTEM);
+    }
+
+    void TearDown() override
+    {
+        dispose((disposable_t*)&prng_opts);
+        dispose((disposable_t*)&alloc_opts);
+    }
+
+    allocator_options_t alloc_opts;
+    vccrypt_prng_options_t prng_opts;
+};
+
+/**
+ * We should be able to get curve25519 options if it has been registered.
+ */
+TEST_F(vccrypt_curve25519_sha512_ref_test, options_init)
+{
+    vccrypt_key_agreement_options_t options;
+
+    //we should be able to initialize options for this algorithm
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_options_init(
+            &options, &alloc_opts, &prng_opts,
+            VCCRYPT_KEY_AGREEMENT_ALGORITHM_CURVE25519_SHA512));
+
+    dispose((disposable_t*)&options);
+}
+
+/**
+ * We should be able to create a curve25519 instance.
+ */
+TEST_F(vccrypt_curve25519_sha512_ref_test, init)
+{
+    vccrypt_key_agreement_options_t options;
+    vccrypt_key_agreement_context_t context;
+
+    //we should be able to initialize options for this algorithm
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_options_init(
+            &options, &alloc_opts, &prng_opts,
+            VCCRYPT_KEY_AGREEMENT_ALGORITHM_CURVE25519_SHA512));
+
+    //we should be able to create an algorithm instance
+    ASSERT_EQ(0, vccrypt_key_agreement_init(&options, &context));
+
+    dispose((disposable_t*)&context);
+    dispose((disposable_t*)&options);
+}
+
+/**
+ * Simple test case from NaCl distribution
+ */
+TEST_F(vccrypt_curve25519_sha512_ref_test, alice_bob)
+{
+    const uint8_t ALICE_PRIVATE[] = {
+        0x77, 0x07, 0x6d, 0x0a, 0x73, 0x18, 0xa5, 0x7d,
+        0x3c, 0x16, 0xc1, 0x72, 0x51, 0xb2, 0x66, 0x45,
+        0xdf, 0x4c, 0x2f, 0x87, 0xeb, 0xc0, 0x99, 0x2a,
+        0xb1, 0x77, 0xfb, 0xa5, 0x1d, 0xb9, 0x2c, 0x2a
+    };
+    const uint8_t ALICE_PUBLIC[] = {
+        0x85, 0x20, 0xf0, 0x09, 0x89, 0x30, 0xa7, 0x54,
+        0x74, 0x8b, 0x7d, 0xdc, 0xb4, 0x3e, 0xf7, 0x5a,
+        0x0d, 0xbf, 0x3a, 0x0d, 0x26, 0x38, 0x1a, 0xf4,
+        0xeb, 0xa4, 0xa9, 0x8e, 0xaa, 0x9b, 0x4e, 0x6a
+    };
+    const uint8_t BOB_PRIVATE[] = {
+        0x5d, 0xab, 0x08, 0x7e, 0x62, 0x4a, 0x8a, 0x4b,
+        0x79, 0xe1, 0x7f, 0x8b, 0x83, 0x80, 0x0e, 0xe6,
+        0x6f, 0x3b, 0xb1, 0x29, 0x26, 0x18, 0xb6, 0xfd,
+        0x1c, 0x2f, 0x8b, 0x27, 0xff, 0x88, 0xe0, 0xeb
+    };
+    const uint8_t BOB_PUBLIC[] = {
+        0xde, 0x9e, 0xdb, 0x7d, 0x7b, 0x7d, 0xc1, 0xb4,
+        0xd3, 0x5b, 0x61, 0xc2, 0xec, 0xe4, 0x35, 0x37,
+        0x3f, 0x83, 0x43, 0xc8, 0x5b, 0x78, 0x67, 0x4d,
+        0xad, 0xfc, 0x7e, 0x14, 0x6f, 0x88, 0x2b, 0x4f
+    };
+    const uint8_t SHARED_SECRET[] = {
+        0x3e, 0xfd, 0xfd, 0x26, 0xb7, 0x19, 0x35, 0xc2,
+        0x6e, 0x47, 0x8d, 0xb0, 0xde, 0x11, 0x88, 0xdf,
+        0x08, 0x5a, 0x91, 0xd0, 0xc6, 0x70, 0xc3, 0x52,
+        0x29, 0x04, 0xd3, 0x11, 0xcc, 0x55, 0x40, 0x04,
+        0x14, 0x39, 0xaa, 0x93, 0x1f, 0xc0, 0xb3, 0xf2,
+        0x70, 0x33, 0x13, 0xd7, 0x2d, 0x6c, 0x11, 0x8c,
+        0x8b, 0x05, 0x56, 0x79, 0xb2, 0xf4, 0xc1, 0x27,
+        0xc2, 0x98, 0x18, 0x71, 0xa1, 0xa6, 0xa0, 0x70
+    };
+
+    vccrypt_key_agreement_options_t options;
+    vccrypt_key_agreement_context_t context;
+
+    //we should be able to initialize options for this algorithm
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_options_init(
+            &options, &alloc_opts, &prng_opts,
+            VCCRYPT_KEY_AGREEMENT_ALGORITHM_CURVE25519_SHA512));
+
+    //we should be able to create an algorithm instance
+    ASSERT_EQ(0, vccrypt_key_agreement_init(&options, &context));
+
+    //create buffers for public and private keys
+    vccrypt_buffer_t alice_private, alice_public, bob_private, bob_public;
+    vccrypt_buffer_t shared;
+    ASSERT_EQ(0, vccrypt_buffer_init(&alice_private, &alloc_opts, 32));
+    memcpy(alice_private.data, ALICE_PRIVATE, 32);
+    ASSERT_EQ(0, vccrypt_buffer_init(&alice_public, &alloc_opts, 32));
+    memcpy(alice_public.data, ALICE_PUBLIC, 32);
+    ASSERT_EQ(0, vccrypt_buffer_init(&bob_private, &alloc_opts, 32));
+    memcpy(bob_private.data, BOB_PRIVATE, 32);
+    ASSERT_EQ(0, vccrypt_buffer_init(&bob_public, &alloc_opts, 32));
+    memcpy(bob_public.data, BOB_PUBLIC, 32);
+    ASSERT_EQ(0, vccrypt_buffer_init(&shared, &alloc_opts, 64));
+
+    //generate the alice-bob shared secret
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_long_term_secret_create(
+            &context, &alice_private, &bob_public, &shared));
+
+    //this should match our precomputed secret
+    ASSERT_EQ(0, memcmp(shared.data, SHARED_SECRET, 64));
+
+    //generate the bob-alice shared secret
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_long_term_secret_create(
+            &context, &bob_private, &alice_public, &shared));
+
+    //this should match our precomputed secret
+    ASSERT_EQ(0, memcmp(shared.data, SHARED_SECRET, 64));
+
+    dispose((disposable_t*)&alice_private);
+    dispose((disposable_t*)&alice_public);
+    dispose((disposable_t*)&bob_private);
+    dispose((disposable_t*)&bob_public);
+    dispose((disposable_t*)&shared);
+    dispose((disposable_t*)&context);
+    dispose((disposable_t*)&options);
+}
+
+/**
+ * Test of the long-term key derivation.
+ */
+TEST_F(vccrypt_curve25519_sha512_ref_test, alice_bob_short_term)
+{
+    const uint8_t ALICE_PRIVATE[] = {
+        0x77, 0x07, 0x6d, 0x0a, 0x73, 0x18, 0xa5, 0x7d,
+        0x3c, 0x16, 0xc1, 0x72, 0x51, 0xb2, 0x66, 0x45,
+        0xdf, 0x4c, 0x2f, 0x87, 0xeb, 0xc0, 0x99, 0x2a,
+        0xb1, 0x77, 0xfb, 0xa5, 0x1d, 0xb9, 0x2c, 0x2a
+    };
+    const uint8_t ALICE_PUBLIC[] = {
+        0x85, 0x20, 0xf0, 0x09, 0x89, 0x30, 0xa7, 0x54,
+        0x74, 0x8b, 0x7d, 0xdc, 0xb4, 0x3e, 0xf7, 0x5a,
+        0x0d, 0xbf, 0x3a, 0x0d, 0x26, 0x38, 0x1a, 0xf4,
+        0xeb, 0xa4, 0xa9, 0x8e, 0xaa, 0x9b, 0x4e, 0x6a
+    };
+    const uint8_t ALICE_NONCE[] = {
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+    const uint8_t BOB_PRIVATE[] = {
+        0x5d, 0xab, 0x08, 0x7e, 0x62, 0x4a, 0x8a, 0x4b,
+        0x79, 0xe1, 0x7f, 0x8b, 0x83, 0x80, 0x0e, 0xe6,
+        0x6f, 0x3b, 0xb1, 0x29, 0x26, 0x18, 0xb6, 0xfd,
+        0x1c, 0x2f, 0x8b, 0x27, 0xff, 0x88, 0xe0, 0xeb
+    };
+    const uint8_t BOB_PUBLIC[] = {
+        0xde, 0x9e, 0xdb, 0x7d, 0x7b, 0x7d, 0xc1, 0xb4,
+        0xd3, 0x5b, 0x61, 0xc2, 0xec, 0xe4, 0x35, 0x37,
+        0x3f, 0x83, 0x43, 0xc8, 0x5b, 0x78, 0x67, 0x4d,
+        0xad, 0xfc, 0x7e, 0x14, 0x6f, 0x88, 0x2b, 0x4f
+    };
+    const uint8_t BOB_NONCE[] = {
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+    const uint8_t SHARED_SECRET[] = {
+        0xf7, 0x2a, 0x43, 0x4c, 0xbb, 0xeb, 0xd5, 0x7c,
+        0x20, 0x75, 0x66, 0x79, 0x75, 0xd5, 0xe2, 0x8a,
+        0x1a, 0xa5, 0x09, 0x92, 0xae, 0xea, 0x5c, 0x81,
+        0x40, 0x0e, 0x5c, 0x71, 0x28, 0xf4, 0x38, 0xef,
+        0x52, 0x5c, 0x76, 0x7d, 0x44, 0x3a, 0x29, 0xa8,
+        0x09, 0x40, 0xce, 0x7a, 0x27, 0xca, 0xb7, 0xaa,
+        0x22, 0x18, 0x56, 0x39, 0xf9, 0x81, 0x98, 0xf0,
+        0x43, 0x66, 0x99, 0xd1, 0xb4, 0x8c, 0x90, 0x8b
+    };
+
+    vccrypt_key_agreement_options_t options;
+    vccrypt_key_agreement_context_t context;
+
+    //we should be able to initialize options for this algorithm
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_options_init(
+            &options, &alloc_opts, &prng_opts,
+            VCCRYPT_KEY_AGREEMENT_ALGORITHM_CURVE25519_SHA512));
+
+    //we should be able to create an algorithm instance
+    ASSERT_EQ(0, vccrypt_key_agreement_init(&options, &context));
+
+    //create buffers for public and private keys
+    vccrypt_buffer_t alice_private, alice_public, alice_nonce;
+    vccrypt_buffer_t bob_private, bob_public, bob_nonce;
+    vccrypt_buffer_t shared;
+    ASSERT_EQ(0, vccrypt_buffer_init(&alice_private, &alloc_opts, 32));
+    memcpy(alice_private.data, ALICE_PRIVATE, 32);
+    ASSERT_EQ(0, vccrypt_buffer_init(&alice_public, &alloc_opts, 32));
+    memcpy(alice_public.data, ALICE_PUBLIC, 32);
+    ASSERT_EQ(0, vccrypt_buffer_init(&alice_nonce, &alloc_opts, 64));
+    memcpy(alice_nonce.data, ALICE_NONCE, 64);
+    ASSERT_EQ(0, vccrypt_buffer_init(&bob_private, &alloc_opts, 32));
+    memcpy(bob_private.data, BOB_PRIVATE, 32);
+    ASSERT_EQ(0, vccrypt_buffer_init(&bob_public, &alloc_opts, 32));
+    memcpy(bob_public.data, BOB_PUBLIC, 32);
+    ASSERT_EQ(0, vccrypt_buffer_init(&bob_nonce, &alloc_opts, 64));
+    memcpy(bob_nonce.data, BOB_NONCE, 64);
+    ASSERT_EQ(0, vccrypt_buffer_init(&shared, &alloc_opts, 64));
+
+    //generate the alice-bob shared secret
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_short_term_secret_create(
+            &context, &alice_private, &bob_public, &alice_nonce, &bob_nonce,
+            &shared));
+
+    //this should match our precomputed secret
+    ASSERT_EQ(0, memcmp(shared.data, SHARED_SECRET, 64));
+
+    //generate the bob-alice shared secret
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_short_term_secret_create(
+            &context, &bob_private, &alice_public, &alice_nonce, &bob_nonce,
+            &shared));
+
+    //this should match our precomputed secret
+    ASSERT_EQ(0, memcmp(shared.data, SHARED_SECRET, 64));
+
+    dispose((disposable_t*)&alice_private);
+    dispose((disposable_t*)&alice_public);
+    dispose((disposable_t*)&alice_nonce);
+    dispose((disposable_t*)&bob_private);
+    dispose((disposable_t*)&bob_public);
+    dispose((disposable_t*)&bob_nonce);
+    dispose((disposable_t*)&shared);
+    dispose((disposable_t*)&context);
+    dispose((disposable_t*)&options);
+}
+
+/**
+ * Test that two randomly generated keypairs produce the same key.
+ */
+TEST_F(vccrypt_curve25519_sha512_ref_test, random)
+{
+    vccrypt_key_agreement_options_t options;
+    vccrypt_key_agreement_context_t context;
+
+    //we should be able to initialize options for this algorithm
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_options_init(
+            &options, &alloc_opts, &prng_opts,
+            VCCRYPT_KEY_AGREEMENT_ALGORITHM_CURVE25519_SHA512));
+
+    //we should be able to create an algorithm instance
+    ASSERT_EQ(0, vccrypt_key_agreement_init(&options, &context));
+
+    //create buffers for public and private keys
+    vccrypt_buffer_t alice_private, alice_public, bob_private, bob_public;
+    vccrypt_buffer_t ab_shared, ba_shared;
+    ASSERT_EQ(0, vccrypt_buffer_init(&alice_private, &alloc_opts, 32));
+    ASSERT_EQ(0, vccrypt_buffer_init(&alice_public, &alloc_opts, 32));
+    ASSERT_EQ(0, vccrypt_buffer_init(&bob_private, &alloc_opts, 32));
+    ASSERT_EQ(0, vccrypt_buffer_init(&bob_public, &alloc_opts, 32));
+    ASSERT_EQ(0, vccrypt_buffer_init(&ab_shared, &alloc_opts, 64));
+    ASSERT_EQ(0, vccrypt_buffer_init(&ba_shared, &alloc_opts, 64));
+
+    //generate alice's keypair
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_keypair_create(
+            &context, &alice_private, &alice_public));
+
+    //generate bob's keypair
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_keypair_create(
+            &context, &bob_private, &bob_public));
+
+    //generate the alice-bob shared secret
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_long_term_secret_create(
+            &context, &alice_private, &bob_public, &ab_shared));
+
+    //generate the bob-alice shared secret
+    ASSERT_EQ(0,
+        vccrypt_key_agreement_long_term_secret_create(
+            &context, &bob_private, &alice_public, &ba_shared));
+
+    //the two shared secrets should match
+    ASSERT_EQ(0, memcmp(ab_shared.data, ba_shared.data, 64));
+
+    dispose((disposable_t*)&alice_private);
+    dispose((disposable_t*)&alice_public);
+    dispose((disposable_t*)&bob_private);
+    dispose((disposable_t*)&bob_public);
+    dispose((disposable_t*)&ab_shared);
+    dispose((disposable_t*)&ba_shared);
+    dispose((disposable_t*)&context);
+    dispose((disposable_t*)&options);
+}
