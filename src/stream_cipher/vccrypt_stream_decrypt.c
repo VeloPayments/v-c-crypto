f989a16e214d08c68a8d7f77389d9832ce4aa2d5
Justin Handville <nanolith@gmail.com>
2018-03-01 01:35:22 -0500

    Fixed model assertion.

--- src/stream_cipher/vccrypt_stream_decrypt.c
+++ src/stream_cipher/vccrypt_stream_decrypt.c
@@ -30,7 +30,7 @@ int vccrypt_stream_decrypt(
 {
     MODEL_ASSERT(NULL != context);
     MODEL_ASSERT(NULL != context->options);
-    MODEL_ASSERT(NULL != context->options->vccrypt_aes_ctr_alg_encrypt);
+    MODEL_ASSERT(NULL != context->options->vccrypt_aes_ctr_alg_decrypt);
     MODEL_ASSERT(NULL != input);
     MODEL_ASSERT(0 < size);
     MODEL_ASSERT(NULL != output);
