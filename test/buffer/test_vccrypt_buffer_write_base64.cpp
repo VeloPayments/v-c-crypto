83a613403f1909906b7f69fc64801d22bbfde815
Justin Handville <nanolith@gmail.com>
2023-08-17 20:27:44 -0400

    Converted unit tests to minunit.

--- test/buffer/test_vccrypt_buffer_write_base64.cpp
+++ test/buffer/test_vccrypt_buffer_write_base64.cpp
@@ -3,26 +3,25 @@
  *
  * Unit tests for vccrypt_buffer_write_base64.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <iostream>
+#include <minunit/minunit.h>
+#include <string.h>
 #include <vpr/allocator/malloc_allocator.h>
 #include <vccrypt/buffer.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
 
-class vccrypt_buffer_write_base64_test : public ::testing::Test {
-protected:
-    void SetUp() override
+class vccrypt_buffer_write_base64_test {
+public:
+    void setUp()
     {
         malloc_allocator_options_init(&alloc_opts);
     }
 
-    void TearDown() override
+    void tearDown()
     {
         dispose((disposable_t*)&alloc_opts);
     }
@@ -30,11 +29,22 @@ protected:
     allocator_options_t alloc_opts;
 };
 
+TEST_SUITE(vccrypt_buffer_write_base64_test);
+
+#define BEGIN_TEST_F(name) \
+TEST(name) \
+{ \
+    vccrypt_buffer_write_base64_test fixture; \
+    fixture.setUp();
+
+#define END_TEST_F() \
+    fixture.tearDown(); \
+}
+
 /**
  * Test that we can write base64 values.
  */
-TEST_F(vccrypt_buffer_write_base64_test, simple_test)
-{
+BEGIN_TEST_F(simple_test)
     bool failFlag = false;
     const char* INPUTS[] = {
         "f",
@@ -74,17 +84,22 @@ TEST_F(vccrypt_buffer_write_base64_test, simple_test)
     for (size_t i = 0; i < sizeof(INPUTS) / sizeof(const char*); ++i)
     {
         //create source buffer
-        ASSERT_EQ(0, vccrypt_buffer_init(&source, &alloc_opts, INPUT_LENS[i]));
+        TEST_ASSERT(
+            0 == vccrypt_buffer_init(
+                    &source, &fixture.alloc_opts, INPUT_LENS[i]));
 
         //set data in source buffer
         memcpy(source.data, INPUTS[i], INPUT_LENS[i]);
 
         //create destination buffer
-        ASSERT_EQ(0, vccrypt_buffer_init_for_base64_serialization(&dest, &alloc_opts, INPUT_LENS[i]));
+        TEST_ASSERT(
+            0
+                == vccrypt_buffer_init_for_base64_serialization(
+                        &dest, &fixture.alloc_opts, INPUT_LENS[i]));
 
         //write Base64 data to output buffer.
         //buffer.
-        ASSERT_EQ(0, vccrypt_buffer_write_base64(&dest, &source));
+        TEST_ASSERT(0 == vccrypt_buffer_write_base64(&dest, &source));
 
         if (memcmp(dest.data, OUTPUTS[i], dest.size))
         {
@@ -103,27 +118,28 @@ TEST_F(vccrypt_buffer_write_base64_test, simple_test)
     }
 
     if (failFlag)
-        FAIL();
-}
+        TEST_FAILURE();
+END_TEST_F()
 
 /**
  * Test that a size mismatch results in an error.
  */
-TEST_F(vccrypt_buffer_write_base64_test, size_mismatch)
-{
+BEGIN_TEST_F(size_mismatch)
     vccrypt_buffer_t source, dest;
 
     //create source buffer
-    ASSERT_EQ(0, vccrypt_buffer_init(&source, &alloc_opts, 32));
+    TEST_ASSERT(0 == vccrypt_buffer_init(&source, &fixture.alloc_opts, 32));
 
     //create destination buffer
-    ASSERT_EQ(0, vccrypt_buffer_init_for_base64_serialization(&dest, &alloc_opts, 30));
+    TEST_ASSERT(
+        0
+            == vccrypt_buffer_init_for_base64_serialization(
+                    &dest, &fixture.alloc_opts, 30));
 
     //the base64 write fails, because dest.size is too small.
-    EXPECT_NE(0, vccrypt_buffer_write_base64(&dest, &source));
+    TEST_EXPECT(0 != vccrypt_buffer_write_base64(&dest, &source));
 
     //dispose buffers
     dispose((disposable_t*)&source);
     dispose((disposable_t*)&dest);
-}
-#endif
+END_TEST_F()
