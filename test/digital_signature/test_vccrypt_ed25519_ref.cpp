a62567c60644ff9b5fb1ef0afbe4c01dde2a79cc
Justin Handville <nanolith@gmail.com>
2023-08-18 00:46:11 -0400

    Converted unit tests to minunit.

--- test/digital_signature/test_vccrypt_ed25519_ref.cpp
+++ test/digital_signature/test_vccrypt_ed25519_ref.cpp
@@ -3,23 +3,21 @@
  *
  * Unit tests for the reference ed25519 implementation.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
 #include <fstream>
+#include <minunit/minunit.h>
 #include <sstream>
-
+#include <string.h>
 #include <vccrypt/digital_signature.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
 
-class vccrypt_ed25519_ref_test : public ::testing::Test {
-protected:
-    void SetUp() override
+class vccrypt_ed25519_ref_test {
+public:
+    void setUp()
     {
         //make sure our prng has been registered
         vccrypt_digital_signature_register_ed25519();
@@ -33,7 +31,7 @@ protected:
                 &prng_opts, &alloc_opts, VCCRYPT_PRNG_SOURCE_OPERATING_SYSTEM);
     }
 
-    void TearDown() override
+    void tearDown()
     {
         if (VCCRYPT_STATUS_SUCCESS == vccrypt_prng_options_init_status)
         {
@@ -47,64 +45,76 @@ protected:
     vccrypt_prng_options_t prng_opts;
 };
 
+TEST_SUITE(vccrypt_ed25519_ref_test);
+
+#define BEGIN_TEST_F(name) \
+TEST(name) \
+{ \
+    vccrypt_ed25519_ref_test fixture; \
+    fixture.setUp();
+
+#define END_TEST_F() \
+    fixture.tearDown(); \
+}
+
 /**
  * Verify that vccrypt_prng_options_init executed successfully.
  */
-TEST_F(vccrypt_ed25519_ref_test, prng_options_init)
-{
-    ASSERT_EQ(VCCRYPT_STATUS_SUCCESS, vccrypt_prng_options_init_status);
-}
+BEGIN_TEST_F(prng_options_init)
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS == fixture.vccrypt_prng_options_init_status);
+END_TEST_F()
 
 /**
  * We should be able to get ed25519 options if it has been registered.
  */
-TEST_F(vccrypt_ed25519_ref_test, options_init)
-{
+BEGIN_TEST_F(options_init)
     vccrypt_digital_signature_options_t options;
 
     //we should be able to initialize options for this algorithm
-    ASSERT_EQ(0,
-        vccrypt_digital_signature_options_init(
-            &options, &alloc_opts, &prng_opts,
-            VCCRYPT_DIGITAL_SIGNATURE_ALGORITHM_ED25519));
+    TEST_ASSERT(
+        0
+            == vccrypt_digital_signature_options_init(
+                    &options, &fixture.alloc_opts, &fixture.prng_opts,
+                    VCCRYPT_DIGITAL_SIGNATURE_ALGORITHM_ED25519));
 
     dispose((disposable_t*)&options);
-}
+END_TEST_F()
 
 /**
  * We should be able to create an ed25519 instance.
  */
-TEST_F(vccrypt_ed25519_ref_test, init)
-{
+BEGIN_TEST_F(init)
     vccrypt_digital_signature_options_t options;
     vccrypt_digital_signature_context_t context;
 
     //we should be able to initialize options for this algorithm
-    ASSERT_EQ(0,
-        vccrypt_digital_signature_options_init(
-            &options, &alloc_opts, &prng_opts,
-            VCCRYPT_DIGITAL_SIGNATURE_ALGORITHM_ED25519));
+    TEST_ASSERT(
+        0
+            == vccrypt_digital_signature_options_init(
+                    &options, &fixture.alloc_opts, &fixture.prng_opts,
+                    VCCRYPT_DIGITAL_SIGNATURE_ALGORITHM_ED25519));
 
     //we should be able to create an algorithm instance
-    ASSERT_EQ(0, vccrypt_digital_signature_init(&options, &context));
+    TEST_ASSERT(0 == vccrypt_digital_signature_init(&options, &context));
 
     dispose((disposable_t*)&context);
     dispose((disposable_t*)&options);
-}
+END_TEST_F()
 
 /**
  * Test the signature test vectors.
  */
-TEST_F(vccrypt_ed25519_ref_test, simple_sign)
-{
+BEGIN_TEST_F(simple_sign)
     vccrypt_digital_signature_options_t options;
     vccrypt_digital_signature_context_t context;
 
     //we should be able to initialize options for this algorithm
-    ASSERT_EQ(0,
-        vccrypt_digital_signature_options_init(
-            &options, &alloc_opts, &prng_opts,
-            VCCRYPT_DIGITAL_SIGNATURE_ALGORITHM_ED25519));
+    TEST_ASSERT(
+        0
+            == vccrypt_digital_signature_options_init(
+                    &options, &fixture.alloc_opts, &fixture.prng_opts,
+                    VCCRYPT_DIGITAL_SIGNATURE_ALGORITHM_ED25519));
 
     // By default the file is relative to the project root.
     // You can set TEST_SIGNATURE_FILE to an absolute path to the file to test from anywhere.
@@ -120,7 +130,7 @@ TEST_F(vccrypt_ed25519_ref_test, simple_sign)
     ifstream in(sign_path);
 
     //this stream should be valid
-    ASSERT_TRUE(in.good()) << "Using path: " << sign_path;
+    TEST_ASSERT(in.good());
 
     //iterate through all test vectors
     string line, priv, pub, msg, signature;
@@ -129,90 +139,112 @@ TEST_F(vccrypt_ed25519_ref_test, simple_sign)
         stringstream linein(line);
 
         //read the components for this test vector
-        ASSERT_TRUE(getline(linein, priv, ':'));
-        ASSERT_TRUE(getline(linein, pub, ':'));
-        ASSERT_TRUE(getline(linein, msg, ':'));
-        ASSERT_TRUE(getline(linein, signature, ':'));
+        TEST_ASSERT(getline(linein, priv, ':'));
+        TEST_ASSERT(getline(linein, pub, ':'));
+        TEST_ASSERT(getline(linein, msg, ':'));
+        TEST_ASSERT(getline(linein, signature, ':'));
 
         //the private key should be 128 digits long
-        ASSERT_EQ(128U, priv.size());
+        TEST_ASSERT(128U == priv.size());
         //the public key should be 64 digits long
-        ASSERT_EQ(64U, pub.size());
+        TEST_ASSERT(64U == pub.size());
         //the signature should be at least 128 digits long
-        ASSERT_LE(128U, signature.size());
+        TEST_ASSERT(128U <= signature.size());
 
         //convert the private key to a buffer
         vccrypt_buffer_t priv_hex;
-        ASSERT_EQ(0, vccrypt_buffer_init(&priv_hex, &alloc_opts, priv.size()));
-        ASSERT_EQ(0,
-            vccrypt_buffer_read_data(&priv_hex, priv.c_str(), priv.size()));
+        TEST_ASSERT(
+            0 == vccrypt_buffer_init(
+                    &priv_hex, &fixture.alloc_opts, priv.size()));
+        TEST_ASSERT(
+            0
+                == vccrypt_buffer_read_data(
+                        &priv_hex, priv.c_str(), priv.size()));
 
         //convert the private hex data to binary
         vccrypt_buffer_t priv_bytes;
-        ASSERT_EQ(0, vccrypt_buffer_init(&priv_bytes, &alloc_opts, 64));
-        ASSERT_EQ(0,
-            vccrypt_buffer_read_hex(&priv_bytes, &priv_hex));
+        TEST_ASSERT(
+            0 == vccrypt_buffer_init(&priv_bytes, &fixture.alloc_opts, 64));
+        TEST_ASSERT(0 == vccrypt_buffer_read_hex(&priv_bytes, &priv_hex));
 
         //convert the public key to a buffer
         vccrypt_buffer_t pub_hex;
-        ASSERT_EQ(0, vccrypt_buffer_init(&pub_hex, &alloc_opts, pub.size()));
-        ASSERT_EQ(0,
-            vccrypt_buffer_read_data(&pub_hex, pub.c_str(), pub.size()));
+        TEST_ASSERT(
+            0
+                == vccrypt_buffer_init(
+                        &pub_hex, &fixture.alloc_opts, pub.size()));
+        TEST_ASSERT(
+            0 == vccrypt_buffer_read_data(&pub_hex, pub.c_str(), pub.size()));
 
         //convert the public hex data to binary
         vccrypt_buffer_t pub_bytes;
-        ASSERT_EQ(0, vccrypt_buffer_init(&pub_bytes, &alloc_opts, 32));
-        ASSERT_EQ(0,
-            vccrypt_buffer_read_hex(&pub_bytes, &pub_hex));
+        TEST_ASSERT(
+            0 == vccrypt_buffer_init(&pub_bytes, &fixture.alloc_opts, 32));
+        TEST_ASSERT(0 == vccrypt_buffer_read_hex(&pub_bytes, &pub_hex));
 
         //convert the message to a buffer
         vccrypt_buffer_t msg_hex;
-        ASSERT_EQ(0, vccrypt_buffer_init(&msg_hex, &alloc_opts, msg.size()));
-        ASSERT_EQ(0,
-            vccrypt_buffer_read_data(&msg_hex, msg.c_str(), msg.size()));
+        TEST_ASSERT(
+            0
+                == vccrypt_buffer_init(
+                        &msg_hex, &fixture.alloc_opts, msg.size()));
+        TEST_ASSERT(
+            0 == vccrypt_buffer_read_data(&msg_hex, msg.c_str(), msg.size()));
 
         //convert the message hex data to binary
         vccrypt_buffer_t msg_bytes;
-        ASSERT_EQ(0, vccrypt_buffer_init(&msg_bytes, &alloc_opts, msg.size() / 2));
-        ASSERT_EQ(0,
-            vccrypt_buffer_read_hex(&msg_bytes, &msg_hex));
+        TEST_ASSERT(
+            0
+                == vccrypt_buffer_init(
+                        &msg_bytes, &fixture.alloc_opts, msg.size() / 2));
+        TEST_ASSERT(0 == vccrypt_buffer_read_hex(&msg_bytes, &msg_hex));
 
         //convert the signature to a buffer
         vccrypt_buffer_t sign_hex;
-        ASSERT_EQ(0, vccrypt_buffer_init(&sign_hex, &alloc_opts, signature.size()));
-        ASSERT_EQ(0,
-            vccrypt_buffer_read_data(
-                &sign_hex, signature.c_str(), signature.size()));
+        TEST_ASSERT(
+            0
+                == vccrypt_buffer_init(
+                        &sign_hex, &fixture.alloc_opts, signature.size()));
+        TEST_ASSERT(
+            0
+                == vccrypt_buffer_read_data(
+                        &sign_hex, signature.c_str(), signature.size()));
 
         //convert the signature hex data to binary
         vccrypt_buffer_t sign_bytes;
-        ASSERT_EQ(0, vccrypt_buffer_init(&sign_bytes, &alloc_opts, signature.size() / 2));
-        ASSERT_EQ(0,
-            vccrypt_buffer_read_hex(&sign_bytes, &sign_hex));
+        TEST_ASSERT(
+            0
+                == vccrypt_buffer_init(
+                        &sign_bytes, &fixture.alloc_opts,
+                        signature.size() / 2));
+        TEST_ASSERT(0 == vccrypt_buffer_read_hex(&sign_bytes, &sign_hex));
 
         //create a buffer to hold the generated signature
         vccrypt_buffer_t sign_buffer;
-        ASSERT_EQ(0, vccrypt_buffer_init(&sign_buffer, &alloc_opts, 64));
+        TEST_ASSERT(
+            0 == vccrypt_buffer_init(&sign_buffer, &fixture.alloc_opts, 64));
 
         //create the digital signature context
-        ASSERT_EQ(0, vccrypt_digital_signature_init(&options, &context));
+        TEST_ASSERT(0 == vccrypt_digital_signature_init(&options, &context));
 
         //sign the message
-        ASSERT_EQ(0,
-            vccrypt_digital_signature_sign(
-                &context, &sign_buffer, &priv_bytes,
-                (const uint8_t*)msg_bytes.data, msg_bytes.size));
+        TEST_ASSERT(
+            0
+                == vccrypt_digital_signature_sign(
+                        &context, &sign_buffer, &priv_bytes,
+                        (const uint8_t*)msg_bytes.data, msg_bytes.size));
 
         //verify that our generated signature matches the provided signature
         //NOTE: this is an UNSAFE WAY to compare signatures.  Used here only for
         //testing purposes.  Use a constant-time comparison in real code!
-        ASSERT_EQ(0, memcmp(sign_buffer.data, sign_bytes.data, 64));
+        TEST_ASSERT(0 == memcmp(sign_buffer.data, sign_bytes.data, 64));
 
         //verify the signature
-        ASSERT_EQ(0,
-            vccrypt_digital_signature_verify(
-                &context, &sign_bytes, &pub_bytes,
-                (const uint8_t*)msg_bytes.data, msg_bytes.size));
+        TEST_ASSERT(
+            0
+                == vccrypt_digital_signature_verify(
+                        &context, &sign_bytes, &pub_bytes,
+                        (const uint8_t*)msg_bytes.data, msg_bytes.size));
 
         //dispose the digital signature context
         dispose((disposable_t*)&context);
@@ -231,54 +263,55 @@ TEST_F(vccrypt_ed25519_ref_test, simple_sign)
 
 
     dispose((disposable_t*)&options);
-}
+END_TEST_F()
 
 /**
  * Test that we can generate a random keypair, sign a message, and verify the
  * message.
  */
-TEST_F(vccrypt_ed25519_ref_test, keygen_sign)
-{
+BEGIN_TEST_F(keygen_sign)
     const uint8_t message[] = "foo bar baz";
     vccrypt_digital_signature_options_t options;
     vccrypt_digital_signature_context_t context;
 
     //we should be able to initialize options for this algorithm
-    ASSERT_EQ(0,
-        vccrypt_digital_signature_options_init(
-            &options, &alloc_opts, &prng_opts,
-            VCCRYPT_DIGITAL_SIGNATURE_ALGORITHM_ED25519));
+    TEST_ASSERT(
+        0
+            == vccrypt_digital_signature_options_init(
+                    &options, &fixture.alloc_opts, &fixture.prng_opts,
+                    VCCRYPT_DIGITAL_SIGNATURE_ALGORITHM_ED25519));
 
     //create a buffer for the private key
     vccrypt_buffer_t priv;
-    ASSERT_EQ(0, vccrypt_buffer_init(&priv, &alloc_opts, 64));
+    TEST_ASSERT(0 == vccrypt_buffer_init(&priv, &fixture.alloc_opts, 64));
 
     //create a buffer for the public key
     vccrypt_buffer_t pub;
-    ASSERT_EQ(0, vccrypt_buffer_init(&pub, &alloc_opts, 32));
+    TEST_ASSERT(0 == vccrypt_buffer_init(&pub, &fixture.alloc_opts, 32));
 
     //create a buffer for the signature
     vccrypt_buffer_t signature;
-    ASSERT_EQ(0, vccrypt_buffer_init(&signature, &alloc_opts, 64));
+    TEST_ASSERT(
+        0 == vccrypt_buffer_init(&signature, &fixture.alloc_opts, 64));
 
     //create the digital signature context
-    ASSERT_EQ(0, vccrypt_digital_signature_init(&options, &context));
+    TEST_ASSERT(0 == vccrypt_digital_signature_init(&options, &context));
 
     //generate a keypair
-    ASSERT_EQ(0,
-        vccrypt_digital_signature_keypair_create(&context, &priv, &pub));
+    TEST_ASSERT(
+        0 == vccrypt_digital_signature_keypair_create(&context, &priv, &pub));
 
     //sign the message
-    ASSERT_EQ(0,
-        vccrypt_digital_signature_sign(
-            &context, &signature, &priv,
-            message, sizeof(message)));
+    TEST_ASSERT(
+        0
+            == vccrypt_digital_signature_sign(
+                    &context, &signature, &priv, message, sizeof(message)));
 
     //verify the signature
-    ASSERT_EQ(0,
-        vccrypt_digital_signature_verify(
-            &context, &signature, &pub,
-            message, sizeof(message)));
+    TEST_ASSERT(
+        0
+            == vccrypt_digital_signature_verify(
+                    &context, &signature, &pub, message, sizeof(message)));
 
     //dispose the digital signature context
     dispose((disposable_t*)&context);
@@ -290,5 +323,4 @@ TEST_F(vccrypt_ed25519_ref_test, keygen_sign)
 
     //dispose of the options
     dispose((disposable_t*)&options);
-}
-#endif
+END_TEST_F()
