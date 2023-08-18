7f73aaea432aeb2a8fd36fb8bcf78eb324b5761a
Justin Handville <nanolith@gmail.com>
2023-08-18 00:33:19 -0400

    Converted unit tests to minunit.

--- test/buffer/test_vccrypt_buffer_write_hex.cpp
+++ test/buffer/test_vccrypt_buffer_write_hex.cpp
@@ -3,23 +3,22 @@
  *
  * Unit tests for vccrypt_buffer_write_hex.
  *
- * \copyright 2017 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2017-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
+#include <minunit/minunit.h>
+#include <string.h>
 #include <vpr/allocator/malloc_allocator.h>
 #include <vccrypt/buffer.h>
 
-/* DISABLED GTEST */
-#if 0
-
-class vccrypt_buffer_write_hex_test : public ::testing::Test {
-protected:
-    void SetUp() override
+class vccrypt_buffer_write_hex_test {
+public:
+    void setUp()
     {
         malloc_allocator_options_init(&alloc_opts);
     }
 
-    void TearDown() override
+    void tearDown()
     {
         dispose((disposable_t*)&alloc_opts);
     }
@@ -27,16 +26,28 @@ protected:
     allocator_options_t alloc_opts;
 };
 
+TEST_SUITE(vccrypt_buffer_write_hex_test);
+
+#define BEGIN_TEST_F(name) \
+TEST(name) \
+{ \
+    vccrypt_buffer_write_hex_test fixture; \
+    fixture.setUp();
+
+#define END_TEST_F() \
+    fixture.tearDown(); \
+}
+
 /**
  * Test that we can write hex values to an output buffer.
  */
-TEST_F(vccrypt_buffer_write_hex_test, simple_test)
-{
+BEGIN_TEST_F(simple_test)
     size_t BUFFER_SIZE = 32;
     vccrypt_buffer_t source, dest;
 
     //create source buffer
-    ASSERT_EQ(0, vccrypt_buffer_init(&source, &alloc_opts, BUFFER_SIZE));
+    TEST_ASSERT(
+        0 == vccrypt_buffer_init(&source, &fixture.alloc_opts, BUFFER_SIZE));
 
     //set data in source buffer
     uint8_t* source_bytes = (uint8_t*)source.data;
@@ -47,43 +58,45 @@ TEST_F(vccrypt_buffer_write_hex_test, simple_test)
     }
 
     //create destination buffer
-    ASSERT_EQ(0,
-        vccrypt_buffer_init_for_hex_serialization(
-            &dest, &alloc_opts, BUFFER_SIZE));
+    TEST_ASSERT(
+        0
+            == vccrypt_buffer_init_for_hex_serialization(
+                    &dest, &fixture.alloc_opts, BUFFER_SIZE));
 
     //write hex digits to the destination buffer
-    EXPECT_EQ(0, vccrypt_buffer_write_hex(&dest, &source));
+    TEST_EXPECT(0 == vccrypt_buffer_write_hex(&dest, &source));
 
     //verify the hex values
-    EXPECT_EQ(0,
-        memcmp(dest.data, "000102030405060708090A0B0C0D0E0F"
-                          "101112131415161718191A1B1C1D1E1F",
-            dest.size));
+    TEST_EXPECT(
+        0
+            == memcmp(
+                    dest.data, "000102030405060708090A0B0C0D0E0F"
+                    "101112131415161718191A1B1C1D1E1F", dest.size));
 
     //dispose buffers
     dispose((disposable_t*)&source);
     dispose((disposable_t*)&dest);
-}
+END_TEST_F()
 
 /**
  * Test that the write fails if the destination buffer is too small.
  */
-TEST_F(vccrypt_buffer_write_hex_test, size_error)
-{
+BEGIN_TEST_F(size_error)
     size_t BUFFER_SIZE = 32;
     vccrypt_buffer_t source, dest;
 
     //create source buffer
-    ASSERT_EQ(0, vccrypt_buffer_init(&source, &alloc_opts, BUFFER_SIZE));
+    TEST_ASSERT(
+        0 == vccrypt_buffer_init(&source, &fixture.alloc_opts, BUFFER_SIZE));
 
     //create destination buffer
-    ASSERT_EQ(0, vccrypt_buffer_init(&dest, &alloc_opts, BUFFER_SIZE));
+    TEST_ASSERT(
+        0 == vccrypt_buffer_init(&dest, &fixture.alloc_opts, BUFFER_SIZE));
 
     //write hex digits to the destination buffer should fail
-    EXPECT_NE(0, vccrypt_buffer_write_hex(&dest, &source));
+    TEST_EXPECT(0 != vccrypt_buffer_write_hex(&dest, &source));
 
     //dispose buffers
     dispose((disposable_t*)&source);
     dispose((disposable_t*)&dest);
-}
-#endif
+END_TEST_F()
