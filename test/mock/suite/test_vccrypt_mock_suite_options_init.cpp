56b58f06effe2aab1fb92e365acc03488cbd166d
Justin Handville <nanolith@gmail.com>
2023-08-19 00:59:07 -0400

    Converted unit tests to minunit.

--- test/mock/suite/test_vccrypt_mock_suite_options_init.cpp
+++ test/mock/suite/test_vccrypt_mock_suite_options_init.cpp
@@ -3,19 +3,19 @@
  *
  * Unit tests for the Velo mock crypto suite options init function.
  *
- * \copyright 2020 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2020-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
+#include <minunit/minunit.h>
 #include <vccrypt/mock_suite.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(vccrypt_mock_suite_options_init);
 
 /**
  * Initialization of the mock crypto suite should succeed.
  */
-TEST(vccrypt_mock_suite_options_init, init)
+TEST(init)
 {
     vccrypt_suite_options_t suite;
     allocator_options_t alloc_opts;
@@ -27,12 +27,11 @@ TEST(vccrypt_mock_suite_options_init, init)
     malloc_allocator_options_init(&alloc_opts);
 
     /* initializing the mock suite should succeed. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_mock_suite_options_init(&suite, &alloc_opts));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_mock_suite_options_init(&suite, &alloc_opts));
 
     /* cleanup. */
     dispose((disposable_t*)&suite);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
