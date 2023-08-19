490491b921332b450f49c1254db12c15b5a822ef
Justin Handville <nanolith@gmail.com>
2023-08-19 01:03:41 -0400

    Converted unit tests to minunit.

--- test/padding/test_vccrypt_buffer_pad.cpp
+++ test/padding/test_vccrypt_buffer_pad.cpp
@@ -3,20 +3,21 @@
  *
  * Unit tests for vccrypt_buffer_pad.
  *
- * \copyright 2020 Velo-Payments, Inc.  All rights reserved.
+ * \copyright 2020-2023 Velo-Payments, Inc.  All rights reserved.
  */
 
+#include <minunit/minunit.h>
+#include <string.h>
 #include <vccrypt/padding.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
+TEST_SUITE(vccrypt_buffer_pad);
 
 /**
  * Test that passing an invalid value to vccrypt_buffer_pad results in
  * VCCRYPT_ERROR_BUFFER_INVALID_ARGUMENT.
  */
-TEST(vccrypt_buffer_pad, parameter_checks)
+TEST(parameter_checks)
 {
     vccrypt_buffer_t buffer;
     allocator_options_t alloc_opts;
@@ -25,23 +26,22 @@ TEST(vccrypt_buffer_pad, parameter_checks)
     malloc_allocator_options_init(&alloc_opts);
 
     /* initialize the buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_buffer_init(&buffer, &alloc_opts, 3));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS == vccrypt_buffer_init(&buffer, &alloc_opts, 3));
 
     /* calling vccrypt_buffer_pad with an invalid argument causes an error. */
-    ASSERT_EQ(
-        VCCRYPT_ERROR_BUFFER_INVALID_ARGUMENT,
-        vccrypt_buffer_pad(nullptr, &alloc_opts, 16));
-    ASSERT_EQ(
-        VCCRYPT_ERROR_BUFFER_INVALID_ARGUMENT,
-        vccrypt_buffer_pad(&buffer, nullptr, 16));
-    ASSERT_EQ(
-        VCCRYPT_ERROR_BUFFER_INVALID_ARGUMENT,
-        vccrypt_buffer_pad(&buffer, &alloc_opts, 256));
-    ASSERT_EQ(
-        VCCRYPT_ERROR_BUFFER_INVALID_ARGUMENT,
-        vccrypt_buffer_pad(&buffer, &alloc_opts, 0));
+    TEST_ASSERT(
+        VCCRYPT_ERROR_BUFFER_INVALID_ARGUMENT
+            == vccrypt_buffer_pad(nullptr, &alloc_opts, 16));
+    TEST_ASSERT(
+        VCCRYPT_ERROR_BUFFER_INVALID_ARGUMENT
+            == vccrypt_buffer_pad(&buffer, nullptr, 16));
+    TEST_ASSERT(
+        VCCRYPT_ERROR_BUFFER_INVALID_ARGUMENT
+            == vccrypt_buffer_pad(&buffer, &alloc_opts, 256));
+    TEST_ASSERT(
+        VCCRYPT_ERROR_BUFFER_INVALID_ARGUMENT
+            == vccrypt_buffer_pad(&buffer, &alloc_opts, 0));
 
     /* cleanup. */
     dispose((disposable_t*)&buffer);
@@ -52,7 +52,7 @@ TEST(vccrypt_buffer_pad, parameter_checks)
  * For any blocksize between 1 and 255 inclusive, for any value equal to or less
  * than the blocksize, the padding is correct.
  */
-TEST(vccrypt_buffer_pad, happy_path)
+TEST(happy_path)
 {
     vccrypt_buffer_t buffer;
     allocator_options_t alloc_opts;
@@ -65,27 +65,27 @@ TEST(vccrypt_buffer_pad, happy_path)
         for (size_t i = 0; i <= blocksize; ++i)
         {
             /* create a buffer of the correct size. */
-            ASSERT_EQ(
-                VCCRYPT_STATUS_SUCCESS,
-                vccrypt_buffer_init(&buffer, &alloc_opts, i));
+            TEST_ASSERT(
+                VCCRYPT_STATUS_SUCCESS
+                    == vccrypt_buffer_init(&buffer, &alloc_opts, i));
 
             /* set the buffer value. */
             memset(buffer.data, 0, buffer.size);
 
             /* PRECONDITION: the buffer size is equal to i. */
-            ASSERT_EQ(i, buffer.size);
+            TEST_ASSERT(i == buffer.size);
 
             /* padding the buffer should succeed. */
-            ASSERT_EQ(
-                VCCRYPT_STATUS_SUCCESS,
-                vccrypt_buffer_pad(&buffer, &alloc_opts, blocksize));
+            TEST_ASSERT(
+                VCCRYPT_STATUS_SUCCESS
+                    == vccrypt_buffer_pad(&buffer, &alloc_opts, blocksize));
 
             /* the buffer pointer is not null. */
-            ASSERT_NE(nullptr, buffer.data);
+            TEST_ASSERT(nullptr != buffer.data);
             /* the resulting buffer size should be greater than i. */
-            ASSERT_GT(buffer.size, i);
+            TEST_ASSERT(buffer.size > i);
             /* the buffer size should be a multiple of blocksize. */
-            ASSERT_EQ(0LU, buffer.size % blocksize);
+            TEST_ASSERT(0LU == buffer.size % blocksize);
 
             /* in the case that i < blocksize... */
             if (i < blocksize)
@@ -94,8 +94,8 @@ TEST(vccrypt_buffer_pad, happy_path)
                 size_t padding_size = blocksize - (i % blocksize);
 
                 /* the buffer size should be equal to blocksize. */
-                ASSERT_EQ(buffer.size, blocksize);
-                ASSERT_GE(buffer.size, padding_size);
+                TEST_ASSERT(buffer.size == blocksize);
+                TEST_ASSERT(buffer.size >= padding_size);
 
                 /* compute the padding byte. */
                 uint8_t padding_byte = (uint8_t)padding_size;
@@ -108,17 +108,17 @@ TEST(vccrypt_buffer_pad, happy_path)
                 for (size_t j = buffer.size - 1;
                      j > buffer.size - padding_size - 1; --j)
                 {
-                    EXPECT_EQ(padding_byte, buf[j]);
+                    TEST_EXPECT(padding_byte == buf[j]);
                 }
             }
             /* i == blocksize. */
             else
             {
                 /* the buffer size should be greater than blocksize. */
-                ASSERT_GT(buffer.size, blocksize);
+                TEST_ASSERT(buffer.size > blocksize);
 
                 /* compute the block size byte. */
-                ASSERT_LT(blocksize, 256);
+                TEST_ASSERT(blocksize < 256);
                 uint8_t block_size_byte = (uint8_t)blocksize;
 
                 /* get a byte pointer for the buffer. */
@@ -129,7 +129,7 @@ TEST(vccrypt_buffer_pad, happy_path)
                 for (size_t j = buffer.size - 1;
                      j > buffer.size - blocksize - 1; --j)
                 {
-                    EXPECT_EQ(block_size_byte, buf[j]);
+                    TEST_EXPECT(block_size_byte == buf[j]);
                 }
             }
 
@@ -146,7 +146,7 @@ TEST(vccrypt_buffer_pad, happy_path)
  * If the buffer size is greater than the block size and not a multiple of it,
  * the padding works as expected.
  */
-TEST(vccrypt_buffer_pad, greater_than_blocksize_padding)
+TEST(greater_than_blocksize_padding)
 {
     vccrypt_buffer_t buffer;
     allocator_options_t alloc_opts;
@@ -158,29 +158,29 @@ TEST(vccrypt_buffer_pad, greater_than_blocksize_padding)
     malloc_allocator_options_init(&alloc_opts);
 
     /* create a buffer of the correct size. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_buffer_init(&buffer, &alloc_opts, BUFFER_SIZE));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_buffer_init(&buffer, &alloc_opts, BUFFER_SIZE));
 
     /* set the buffer value. */
     memset(buffer.data, 0, buffer.size);
 
     /* PRECONDITION: the buffer size is equal to BUFFER_SIZE. */
-    ASSERT_EQ(BUFFER_SIZE, buffer.size);
+    TEST_ASSERT(BUFFER_SIZE == buffer.size);
 
     /* padding the buffer should succeed. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_buffer_pad(&buffer, &alloc_opts, BLOCK_SIZE));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_buffer_pad(&buffer, &alloc_opts, BLOCK_SIZE));
 
     /* the buffer pointer is not null. */
-    ASSERT_NE(nullptr, buffer.data);
+    TEST_ASSERT(nullptr != buffer.data);
     /* the resulting buffer size should be greater than BUFFER_SIZE. */
-    ASSERT_GT(buffer.size, BUFFER_SIZE);
+    TEST_ASSERT(buffer.size > BUFFER_SIZE);
     /* the buffer size should be a multiple of blocksize. */
-    ASSERT_EQ(0LU, buffer.size % BLOCK_SIZE);
+    TEST_ASSERT(0LU == buffer.size % BLOCK_SIZE);
     /* the buffer size should be equal to EXPECTED_PADDED_BUFFER_SIZE. */
-    ASSERT_EQ(EXPECTED_PADDED_BUFFER_SIZE, buffer.size);
+    TEST_ASSERT(EXPECTED_PADDED_BUFFER_SIZE == buffer.size);
 
     /* compute the padding byte. */
     size_t padding_size = BLOCK_SIZE - (BUFFER_SIZE % BLOCK_SIZE);
@@ -192,11 +192,10 @@ TEST(vccrypt_buffer_pad, greater_than_blocksize_padding)
     /* ensure that each padding byte is set correctly. */
     for (size_t i = buffer.size - 1; i > buffer.size - padding_size - 1; --i)
     {
-        EXPECT_EQ(padding_byte, buf[i]);
+        TEST_EXPECT(padding_byte == buf[i]);
     }
 
     /* cleanup. */
     dispose((disposable_t*)&buffer);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
